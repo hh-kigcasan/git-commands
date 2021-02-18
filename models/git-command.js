6cd3f35e02c1fad4a5852b913ff53033afc9ab50
hh-kigcasan <hh.kigcasan@gmail.com>
2021-02-18 22:53:55 +0800

    Dot

--- models/git-command.js
+++ models/git-command.js
@@ -25,11 +25,10 @@ class GitCommand {
             this.staging.push(modified_files[path_file]);
             delete modified_files[path_file];
         }
-        /*
-            For assignment #2:
-            Create logic here and run unit testing.
-            Don't forget to uncomment the unit tests.
-        */
+        else if(path_file == "."){
+            this.staging.push(modified_files);
+            this.working_directory.new_changes = {};
+        }
         else{
             return `Failed to add ${path_file}! File is not modified or missing.`;
         }
